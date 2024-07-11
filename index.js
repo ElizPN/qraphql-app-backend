6d2c6539e1986bdd27c00baffc95ffc5c514edae
ElizPN <pankevych.liza@gmail.com>
2024-07-11 10:07:59 +0200

    create root resolver with functions

--- index.js
+++ index.js
@@ -2,13 +2,39 @@ const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const cors = require('cors')
 const schema = require('./schema')
+const users = [{id: 1, username: "Vasya", age: 25}]
 
 
 
 const app = express()
 app.use(cors())
 
-app.use('/graphql', graphqlHTTP({ graphiql: true, schema }))
+const createUser = (input) => {
+    const id = Date.now()
+    return {
+        id, ...input
+    }
+}
+
+
+const root = {
+    getAllUsers: () => {
+        return users
+    },
+    getUser: ({ id }) => {
+        return users.find(user => user.id == id)
+    },
+    createUser: ({input}) => {
+        const user = createUser(input)
+        users.push(user)
+        return user
+    }
+
+
+}
+
+
+app.use('/graphql', graphqlHTTP({ graphiql: true, schema, rootValue: root }))
 
 
 
